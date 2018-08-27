b0ab9dcb155cc64cadfe44f76c707c89b6205345
Luan Devecchi <gitluan@engineer.com>
2018-08-27 22:05:30 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,17 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+def makeplay(x)
+  return " #{x} "
+end
+
+
+def display_board(state)
+  separator = '|'
+  blank_space = '   '
+  dash = '-----------'
+  puts makeplay(state[0]) + separator + makeplay(state[1]) + separator + makeplay(state[2])
+  puts dash
+  puts makeplay(state[3]) + separator + makeplay(state[4]) + separator + makeplay(state[5])
+  puts dash
+  puts makeplay(state[6]) + separator + makeplay(state[7]) + separator + makeplay(state[8])
 end
 
 # code your input_to_index and move method here!
